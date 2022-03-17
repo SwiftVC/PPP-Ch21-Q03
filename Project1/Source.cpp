73b6f3b7976b0b1f04ab22dbfa642361116e6890
SwiftVC <christoph.swift@gmail.com>
2022-03-17 14:03:30 +0000

    fixed answer, updated comment

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,8 +1,12 @@
 /*
 *	Author:	CS
 *	Date:	2022_03_17	YYYY_MM_DD
+*
+*	Implement STL-style count_if() : where x = count(..), x is the number of occurrences of v in [b:e).
+*	Lesson: Visual studio has xutility included by default which was called instead of my version of count
 * 
-*	Implement STL-style count() : where x = count(..), x is the number of occurrences of v in [b:e).
+*	Properties>C/C++>Advanced>Show includes is accessible from Project1\Project1\x64\Debug\Project1.log
+*	xutlities is included in iostream, which has a version of count in the global namespace.
 */
 
 #include <iostream>
@@ -10,12 +14,14 @@
 #include <list>
 #include <string>
 
-template <typename T>
-int count(T* begin, T* end, T val) {
+template <typename It, typename T>
+int my_count(It begin, It end, T val) {
 	int cnt{ 0 };
-	while (begin != end)
+	while (begin != end) {
 		if (val == *begin)
 			++cnt;
+		++begin;
+	}
 	return cnt;
 }
 
@@ -24,8 +30,9 @@ int main() {
 	std::list<std::string> list{ "alpha", "bravo", "alpha", "alpha", "zulu" };
 
 	std::cout << "The count should return three of int(5) in vect :\n";
-	std::cout << count(vect.begin(), vect.end(), 5) << std::endl;
+	std::cout << my_count(vect.begin(), vect.end(), 5) << std::endl;
 	std::cout << "The count should return three of std::string(\"alpha\") in list : \n";
-	std::cout << count(list.begin(), list.end(), "alpha") << std::endl;
+	std::cout << my_count(list.begin(), list.end(), "alpha") << std::endl;
+
 	return 0;
 }
